255f46bb5019821cf20759112708da0f7ef4a019
Rexayo <abbaabbarex@yahoo.com>
2020-10-30 14:06:29 +0000

    styled navbar, light-dark mode button and added dropdown links

--- script/script.js
+++ script/script.js
@@ -1,8 +1,20 @@
-$('.light').click(function() {
-    $("i", this).toggleClass("fa-toggle-on fa-toggle-off");
-     $(".navbar").toggleClass("navbar-light navbar-dark");
-      $(".navbar").toggleClass("bg-light bg-primary");
-      $(".navbar-brand").toggleClass("text-primary text-white");
-    //   $(".navbar-toggler").toggleClass("blue-bg no-bg ");
+$(".light").click(function () {
+  $("i", this).toggleClass("fa-toggle-on fa-toggle-off");
+  $("i", this).toggleClass("text-primary text-white");
+  $(".navbar").toggleClass("navbar-light navbar-dark");
+  $(".navbar").toggleClass("bg-light bg-primary");
+  $(".navbar-brand").toggleClass("text-primary text-white");
+  $(".menu-icon").toggleClass("text-primary text-light");
+});
 
+$(".menu-icon").click(function () {
+  $("i", this).toggleClass("fa-hand-o-down fa-hand-o-up");
+});
+
+$(document).ready(function () {
+  for (i = 0; i < 20; i++) {
+    $(".row").append('<div class="col grey-bg"><img></div>');
+  };
+  document.getElementByClass("grey-bg").style.width = viewWidth / 20;
+  document.getElementByClass("grey-bg").style.height = viewWidth / 20;
 });
